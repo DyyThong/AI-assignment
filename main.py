aeab462cf4e866a1b38af925f57f8542a12dfa5b
Black-Frost <vovantiendung2001@gmail.com>
2021-09-18 09:47:17 +0700

    Add checking function for stuck blocks.

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
 import sys, pygame
 from pygame import display
 
+#https://baldur.iti.kit.edu/theses/SokobanPortfolio.pdf
+
+WALLS = [[0, 1, 0, 1, 0, 0],
+         [0, 1, 0, 1, 1, 1],
+         [1, 1, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 1],
+         [1, 1, 1, 0, 1, 0],
+         [0, 0, 1, 0, 1, 0]]
+PLAYER = (3, 3)
+BOXES = [(2, 2), (2, 4), (3, 2), (4, 3)]
+GOALS = [(0, 2), (2, 5), (3, 0), (5, 3)]
+
+
+def isStuck(box):
+    #Test if a box is stuck
+    #A box is considered stuck if we can't move it vertically or horizontally
+    (x,y) = box
+    #If there is wall up or below the block, we can't move it vertically
+    if (x > 0 and x < len(WALLS) - 1):
+        isStuckVertical = WALLS[x-1][y] or WALLS[x + 1][y]
+    else:
+        isStuckVertical = 1
+    #If there is wall on the left or right to the block, we can't move it horizontally
+    if (y > 0 and y < len(WALLS) - 1):
+        isStuckHorizontal = WALLS[x][y - 1] or WALLS[x][y + 1]
+    else:
+        isStuckHorizontal = 1
+    return isStuckHorizontal and isStuckVertical
+
+
+def isStuckState(state):
+    # Test if a state is unsolvable
+    # If one box is stuck, and it is not at a goal, then we can't move it anymore
+    # Therefore it's impossible to reach the goal from that state
+    for box in BOXES:
+        if isStuck(box) and (box not in GOALS):
+            return True
+    return False
+
 def solveHeuristic():
     pass
 
 
 # Initialize the pygame
-pygame.init()
+#pygame.init()
 
 # Create the screen
-screen = display.set_mode((800, 600))
+#screen = display.set_mode((800, 600))
 
 while True:
     pass

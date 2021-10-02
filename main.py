582a185cf96ba897842f3d7ac37a74a2c961f3d8
DyyThong <thongphdt@gmail.com>
2021-10-02 07:49:17 +0700

    add

--- main.py
+++ main.py
@@ -1,14 +1,149 @@
 #import sys, pygame
 #from pygame import display
 import queue
-import sys
 import time
 import datetime
+import random, sys, copy, os, pygame
+from pygame.locals import *
+
+#Code for Graphic
+FPS = 30 # frames per second to update the screen
+WINWIDTH = 800 # width of the program's window, in pixels
+WINHEIGHT = 600 # height in pixels
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+# The total width and height of each tile in pixels.
+TILEWIDTH = 50
+TILEHEIGHT = 85
+TILEFLOORHEIGHT = 40
+
+CAM_MOVE_SPEED = 5 # how many pixels per frame the camera moves
+
+# The percentage of outdoor tiles that have additional
+# decoration on them, such as a tree or rock.
+OUTSIDE_DECORATION_PCT = 20
+
+BRIGHTBLUE = (  0, 170, 255)
+WHITE      = (255, 255, 255)
+BGCOLOR = BRIGHTBLUE
+TEXTCOLOR = WHITE
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, IMAGESDICT, TILEMAPPING, OUTSIDEDECOMAPPING, BASICFONT, PLAYERIMAGES, currentImage
+
+    # Pygame initialization and basic set up of the global variables.
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+
+    # Because the Surface object stored in DISPLAYSURF was returned
+    # from the pygame.display.set_mode() function, this is the
+    # Surface object that is drawn to the actual computer screen
+    # when pygame.display.update() is called.
+    DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+
+    pygame.display.set_caption('Star Pusher')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+
+    # A global dict value that will contain all the Pygame
+    # Surface objects returned by pygame.image.load().
+    IMAGESDICT = {'uncovered goal': pygame.image.load('RedSelector.png'),
+                  'covered goal': pygame.image.load('Selector.png'),
+                  'star': pygame.image.load('Star.png'),
+                  'corner': pygame.image.load('Wall_Block_Tall.png'),
+                  'wall': pygame.image.load('Wood_Block_Tall.png'),
+                  'inside floor': pygame.image.load('Plain_Block.png'),
+                  'outside floor': pygame.image.load('Grass_Block.png'),
+                  'title': pygame.image.load('star_title.png'),
+                  'solved': pygame.image.load('star_solved.png'),
+                  'princess': pygame.image.load('princess.png'),
+                  'boy': pygame.image.load('boy.png'),
+                  'catgirl': pygame.image.load('catgirl.png'),
+                  'horngirl': pygame.image.load('horngirl.png'),
+                  'pinkgirl': pygame.image.load('pinkgirl.png'),
+                  'rock': pygame.image.load('Rock.png'),
+                  'short tree': pygame.image.load('Tree_Short.png'),
+                  'tall tree': pygame.image.load('Tree_Tall.png'),
+                  'ugly tree': pygame.image.load('Tree_Ugly.png')}
+
+    TILEMAPPING = {'x': IMAGESDICT['corner'],
+                   '#': IMAGESDICT['wall'],
+                   'o': IMAGESDICT['inside floor'],
+                   ' ': IMAGESDICT['outside floor']}
+    OUTSIDEDECOMAPPING = {'1': IMAGESDICT['rock'],
+                          '2': IMAGESDICT['short tree'],
+                          '3': IMAGESDICT['tall tree'],
+                          '4': IMAGESDICT['ugly tree']}
+    # PLAYERIMAGES is a list of all possible characters the player can be.
+    # currentImage is the index of the player's current player image.
+    currentImage = 0
+    PLAYERIMAGES = [IMAGESDICT['princess'],
+                    IMAGESDICT['boy'],
+                    IMAGESDICT['catgirl'],
+                    IMAGESDICT['horngirl'],
+                    IMAGESDICT['pinkgirl']]
+    startScreen() # show the title screen until the user presses a key
+
+    # Read in the levels from the text file. See the readLevelsFile() for
+    # details on the format of this file and how to make your own levels.
+    #levels = readLevelsFile('starPusherLevels.txt')
+    currentLevelIndex = 0
+
+
+def startScreen():
+    """Display the start screen (which has the title and instructions)
+    until the player presses a key. Returns None."""
+
+    # Position the title image.
+    titleRect = IMAGESDICT['title'].get_rect()
+    topCoord = 50 # topCoord tracks where to position the top of the text
+    titleRect.top = topCoord
+    titleRect.centerx = HALF_WINWIDTH
+    topCoord += titleRect.height
+
+    # Unfortunately, Pygame's font & text system only shows one line at
+    # a time, so we can't use strings with \n newline characters in them.
+    # So we will use a list with each line in it.
+    instructionText = ['Push the stars over the marks.',
+                       'Arrow keys to move, WASD for camera control, P to change character.',
+                       'Backspace to reset level, Esc to quit.',
+                       'N for next level, B to go back a level.']
+
+    # Start with drawing a blank color to the entire window:
+    DISPLAYSURF.fill(BGCOLOR)
+
+    # Draw the title image to the window:
+    DISPLAYSURF.blit(IMAGESDICT['title'], titleRect)
+
+    # Position and draw the text.
+    for i in range(len(instructionText)):
+        instSurf = BASICFONT.render(instructionText[i], 1, TEXTCOLOR)
+        instRect = instSurf.get_rect()
+        topCoord += 10 # 10 pixels will go in between each line of text.
+        instRect.top = topCoord
+        instRect.centerx = HALF_WINWIDTH
+        topCoord += instRect.height # Adjust for the height of the line.
+        DISPLAYSURF.blit(instSurf, instRect)
+
+    while True: # Main loop for the start screen.
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                return # user has pressed a key, so return.
+
+        # Display the DISPLAYSURF contents to the actual screen.
+        pygame.display.update()
+        FPSCLOCK.tick()
+
+
 
 #https://baldur.iti.kit.edu/theses/SokobanPortfolio.pdf
 #https://verificationglasses.wordpress.com/2021/01/17/a-star-sokoban-planning/
 #https://home.cse.ust.hk/~yqsong/teaching/comp3211/projects/2017Fall/G14.pdf
 
+#Code for Algorimth
 WALLS = []
 PLAYER = ()
 BOXES = []
@@ -21,6 +156,7 @@ STATEVISITED = 0
 
 #Create level information from text file
 def parseLevel(filename):
+    main()
     '''Notation:
         - @: agent
         - #: wall
@@ -265,7 +401,9 @@ def printPath(finalState):
 
 #while True:
 #    pass
-
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:

54f509b1c2a2cb18e7453c20cc5173004bb5dacf
Black-Frost <vovantiendung2001@gmail.com>
2021-10-14 11:48:57 +0700

    Delete unnecessary comments

--- main.py
+++ main.py
@@ -311,55 +311,6 @@ def makeMove(mapObj, gameStateObj, playerMoveTo):
         gameStateObj['player'] = (playerx + xOffset, playery + yOffset)
         return True
 
-
-# def startScreen():
-#     """Display the start screen (which has the title and instructions)
-#     until the player presses a key. Returns None."""
-
-#     # Position the title image.
-#     titleRect = IMAGESDICT['title'].get_rect()
-#     topCoord = 50 # topCoord tracks where to position the top of the text
-#     titleRect.top = topCoord
-#     titleRect.centerx = HALF_WINWIDTH
-#     topCoord += titleRect.height
-
-#     # Unfortunately, Pygame's font & text system only shows one line at
-#     # a time, so we can't use strings with \n newline characters in them.
-#     # So we will use a list with each line in it.
-#     instructionText = ['Push the stars over the marks.',
-#                        'Backspace to reset level, Esc to quit.',
-#                        'N for next level, B to go back a level.']
-
-#     # Start with drawing a blank color to the entire window:
-#     DISPLAYSURF.fill(BGCOLOR)
-
-#     # Draw the title image to the window:
-#     DISPLAYSURF.blit(IMAGESDICT['title'], titleRect)
-
-#     # Position and draw the text.
-#     for i in range(len(instructionText)):
-#         instSurf = BASICFONT.render(instructionText[i], 1, TEXTCOLOR)
-#         instRect = instSurf.get_rect()
-#         topCoord += 10 # 10 pixels will go in between each line of text.
-#         instRect.top = topCoord
-#         instRect.centerx = HALF_WINWIDTH
-#         topCoord += instRect.height # Adjust for the height of the line.
-#         DISPLAYSURF.blit(instSurf, instRect)
-
-#     while True: # Main loop for the start screen.
-#         for event in pygame.event.get():
-#             if event.type == QUIT:
-#                 terminate()
-#             elif event.type == KEYDOWN:
-#                 if event.key == K_ESCAPE:
-#                     terminate()
-#                 return # user has pressed a key, so return.
-
-#         # Display the DISPLAYSURF contents to the actual screen.
-#         pygame.display.update()
-#         FPSCLOCK.tick()
-
-
 def readLevelsFile(filename):
     assert os.path.exists(filename), 'Cannot find the level file: %s' % (filename)
     mapFile = open(filename, 'r')
